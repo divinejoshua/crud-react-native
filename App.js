4623d95fb52010f2c2d1dff477f7cf1729dbef50
divinejoshua <divineuche399@gmail.com>
2023-07-24 21:58:57 +0100

    Created stack of colors

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { styles } from './style';
 
 export default function App() {
 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!|</Text>
-      <Text>Open up App.js to start working on your app!|</Text>
+
+      {/* Added the header element  */}
+      <View style={styles.header}>
+        <Text style={styles.heroText}>Home  </Text>
+        <Text style={styles.subText}>Good morning, Divine</Text>
+      </View>
+
+      <View style={styles.homeCardBlack}></View>
+      <View style={styles.homeCardBlue}></View>
+      <View style={styles.homeCardPurple}></View>
+      <View style={styles.homeCardPink}></View>
+
+      {/* <Text>Open up App.js to start working on your app!|</Text> */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style.js
@@ -0,0 +1,79 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      marginTop:70,
+      paddingLeft:20,
+      paddingRight:20,
+    },
+
+    header: {
+      marginTop:0,
+      paddingTop: 10,
+      paddingBottom: 10,
+    },
+
+    heroText: {
+      fontSize:30,
+      fontWeight: 'bold',
+      color : '#020617',
+      // fontFamily: 'Montserrat',
+    },
+
+    subText: {
+      marginTop: 10,
+      fontSize:17,
+      color: '#888',
+    },
+    
+    homeCardBlue: {
+      width: '100%',
+      height: 100,
+      backgroundColor: '#60a5fa',
+      alignSelf: 'center',
+      borderRadius: 5,
+      marginTop:10,
+      borderRadius: 10,
+  
+    },
+
+    homeCardBlack: {
+      width: '100%',
+      height: 100,
+      backgroundColor: '#999',
+      alignSelf: 'center',
+      borderRadius: 5,
+      marginTop:10,
+      borderRadius: 10,
+  
+    },
+
+
+    homeCardPurple: {
+      width: '100%',
+      height: 100,
+      backgroundColor: '#8b5cf6',
+      alignSelf: 'center',
+      borderRadius: 5,
+      marginTop:10,
+      borderRadius: 10,
+  
+    },
+
+    homeCardPink: {
+      width: '100%',
+      height: 100,
+      backgroundColor: '#ec4899',
+      alignSelf: 'center',
+      borderRadius: 5,
+      marginTop:10,
+      borderRadius: 10,
+  
+    },
+
+
+
+    });
+    
+    export { styles }
